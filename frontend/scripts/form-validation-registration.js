5d74c2b47a7d0089d8afaae7e223c9605af9dba7
B G <wcgoodman@gmail.com>
2026-02-25 20:11:42 -0600

    Rearranged some frontend code in preparation for adding index.html --BG

--- frontend/scripts/form-validation-registration.js
+++ frontend/scripts/form-validation-registration.js
@@ -149,7 +149,7 @@ function togglePassword(fieldId) {
             securityAnswer: securityAnswer.value.trim()
         };
 
-        fetch('http://localhost:8080/api/register', {// makes an HTTP request to your backend endpoint
+        fetch('http://localhost:8080/api/auth/register', {// makes an HTTP request to your backend endpoint
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(payload)
