31602d0a8ff3ca561ee0c71957e30126b388e0e4
B G <wgoodman5@my.stlcc.edu>
2026-04-28 17:24:23 -0500

    Sprint 6 - added script to populate lookup tables

---
+++ database/populate_lkp_tables.sql
@@ -0,0 +1,14 @@
+-- populate lkp_security_questions
+INSERT INTO lkp_security_questions
+VALUES
+("What was your first pet's name?"),
+("What is your mother's maiden name?"),
+("What city were you born in?");
+
+-- populate lkp_time_record_types
+INSERT INTO lkp_time_record_types (time_record_type)
+VALUES
+("Employment"),
+("Volunteering"),
+("Education"),
+("Childcare");
